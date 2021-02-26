3316838bbe27729636228ad353b70cbbcd434724
Filippo Garota <filogarota101@gmail.com>
2021-02-26 15:56:52 +0100

    Aggiunte variabili e prezzo del biglietto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-prompt();
+var numeroKilometri = parseInt(prompt('Quanti kilometri vuoi percorrere?'));
+var età = parseInt(prompt('Quanti anni hai?'));
+
+//calcolo del costo del biglietto
+
+var prezzoTotale = numeroKilometri * 0.21;
+console.log(prezzoTotale);
