41605632f4d71be6ea141ce66d3b7a1147b10317
Filippo Garota <filogarota101@gmail.com>
2021-02-26 16:12:35 +0100

    Aggiunto calcolo degli sconti

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 var numeroKilometri = parseInt(prompt('Quanti kilometri vuoi percorrere?'));
-var età = parseInt(prompt('Quanti anni hai?'));
+var age = parseInt(prompt('Quanti anni hai?'));
 
 //calcolo del costo del biglietto
 
 var prezzoTotale = numeroKilometri * 0.21;
-console.log(prezzoTotale);
+document.write (prezzoTotale);
+
+//calcolo degli sconti per età
+
+if (age <= 18) {
+  console.log ((prezzoTotale * 20) / 100);
+} else if (age > 65) {
+  console.log ((prezzoTotale * 40) / 100);
+} else {
+  console.log (prezzoTotale);
+}
