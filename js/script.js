b015991f27567fc2df05681500d8e233e5db4f42
Filippo Garota <filogarota101@gmail.com>
2021-02-26 17:02:45 +0100

    Corretto calcolo sconti

--- js/script.js
+++ js/script.js
@@ -5,12 +5,13 @@ var age = parseInt(prompt('Quanti anni hai?'));
 
 var prezzoTotale = Math.floor(numeroKilometri * 0.21);
 
-document.getElementById('prezzo__intero').innerHTML = prezzoTotale;
+document.getElementById('prezzo__intero').innerHTML = prezzoTotale.toFixed(2) + ' €';
 //calcolo degli sconti per età
 
 if (age <= 18) {
-  document.getElementById('sconto').innerHTML = (prezzoTotale * 20) / 100 ;
+  document.getElementById('sconto').innerHTML = prezzoTotale.toFixed(2) - (prezzoTotale.toFixed(2) * 20 / 100) + ' €';
+  ;
 }
 if (age > 65) {
-  document.getElementById('sconto').innerHTML = (prezzoTotale * 40) / 100 ;
+  document.getElementById('sconto').innerHTML =  prezzoTotale.toFixed(2) - (prezzoTotale.toFixed(2) * 40 / 100) + ' €';
 }
