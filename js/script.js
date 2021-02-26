15171a8e5a39a2c1f37c467d7a9b69913a204d76
Filippo Garota <filogarota101@gmail.com>
2021-02-26 16:38:38 +0100

    Aggiunti prezzi all'html

--- js/script.js
+++ js/script.js
@@ -3,15 +3,14 @@ var age = parseInt(prompt('Quanti anni hai?'));
 
 //calcolo del costo del biglietto
 
-var prezzoTotale = numeroKilometri * 0.21;
-document.write (prezzoTotale);
+var prezzoTotale = Math.floor(numeroKilometri * 0.21);
 
+document.getElementById('prezzo__intero').innerHTML = prezzoTotale;
 //calcolo degli sconti per età
 
 if (age <= 18) {
-  console.log ((prezzoTotale * 20) / 100);
-} else if (age > 65) {
-  console.log ((prezzoTotale * 40) / 100);
-} else {
-  console.log (prezzoTotale);
+  document.getElementById('sconto').innerHTML = (prezzoTotale * 20) / 100 ;
+}
+if (age > 65) {
+  document.getElementById('sconto').innerHTML = (prezzoTotale * 40) / 100 ;
 }
