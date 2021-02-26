5b89518781bb351ebfeca5577c151685e45b8f61
Filippo Garota <filogarota101@gmail.com>
2021-02-26 17:33:24 +0100

    Aggiunto toFixed() al prezzo scontato

--- js/script.js
+++ js/script.js
@@ -6,12 +6,16 @@ var age = parseInt(prompt('Quanti anni hai?'));
 var prezzoTotale = Math.floor(numeroKilometri * 0.21);
 
 document.getElementById('prezzo__intero').innerHTML = prezzoTotale.toFixed(2) + ' €';
+
 //calcolo degli sconti per età
 
+var prezzoScontato;
+
 if (age <= 18) {
-  document.getElementById('sconto').innerHTML = prezzoTotale.toFixed(2) - (prezzoTotale.toFixed(2) * 20 / 100) + ' €';
-  ;
+  prezzoScontato = prezzoTotale - (prezzoTotale * 20 / 100);
 }
 if (age > 65) {
-  document.getElementById('sconto').innerHTML =  prezzoTotale.toFixed(2) - (prezzoTotale.toFixed(2) * 40 / 100) + ' €';
+  prezzoScontato = prezzoTotale - (prezzoTotale * 40 / 100);
 }
+
+document.getElementById('sconto').innerHTML = prezzoScontato.toFixed(2) + ' €';
